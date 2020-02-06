cedd25ff70b16f01bff049332863660893603527
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-02-06 12:37:36 -0600

    modified to make songs appear in body instead of console

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 let friends = ["John", "Jack", "Irving", "Steve", "Bradford"];
 
-for (let i = 0; i < friends.length; i++) {
-    console.log(friends[i].toUpperCase() + ":");
+let button = document.querySelector('button');
+button.addEventListener("click", singSong);
+
+function singSong() {
+    for (let i = 0; i < friends.length; i++) {
+        let div = document.createElement('div');
+        div.className = "friend";
+        let name = document.createElement('h3');
+        name.textContent = friends[i];
+        div.appendChild(name);
+        listLyrics(friends[i], div);
+        document.body.appendChild(div);
+    }
+}
+
+
+function listLyrics(friend, div) {
     for (let j = 99; j > 0; j--) {
+        let p = document.createElement('p');
         let plural = "s";
         let linesleft = (j - 1) + " lines";
         if (j == 1) {
@@ -11,6 +27,8 @@ for (let i = 0; i < friends.length; i++) {
         } else if (j == 2) {
             linesleft = "1 more line";
         }
-        console.log(j + " line" + plural + " of code in the file, " + j + " line" + plural + " of code; " + friends[i] + " strikes one out, clears it all out, " + linesleft + " of code in the file");
+        let text = j + " line" + plural + " of code in the file, " + j + " line" + plural + " of code; " + friend + " strikes one out, clears it all out, " + linesleft + " of code in the file";
+        p.textContent = text;
+        div.appendChild(p);
     }
 }
